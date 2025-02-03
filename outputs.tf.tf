eb72a87c436a03e8b100122be8f2d38643277fef
bobyrand <bobyrandrii2024@gmail.com>
2025-02-03 13:19:09 +0200

    Revert "Restructure terraform code"
    This reverts commit d4bf9f06518e7d8d56c3e712f6aa9e291cc296c2.

--- outputs.tf.tf
+++
@@ -1,4 +0,0 @@
-output "instance_ip" {
-  value       = aws_instance.dev_ops_week2.public_ip
-  description = "Public IP address of the EC2 instance"
-}
--- test_1.tf
+++ test_1.tf
@@ -1,20 +1,21 @@
 provider "aws" {
-  region = var.aws_region
+  region = "eu-north-1"
 }
 
+
 resource "aws_instance" "dev_ops_week2" {
-  ami                         = var.ami_id
-  instance_type               = var.instance_type
-  key_name                    = aws_key_pair.terraform_ec2_key.key_name
+  ami                         = "ami-09a9858973b288bdd"
+  instance_type               = "t3.micro"
+  key_name                    = "terraform_ec2_key"
   user_data                   = file("${path.module}/script.sh")
   associate_public_ip_address = true
-  security_groups             = [aws_security_group.example_sg.name]
+  security_groups             = aws_security_group.example_sg.name
 }
 
 resource "aws_security_group" "example_sg" {
   name        = "example-security-group"
   description = "Allow SSH (home IP), HTTP, and HTTPS inbound; all traffic outbound"
-  vpc_id      = var.vpc_id
+  vpc_id      = "vpc-0b6c4a4da62ad49b6" # Укажите ID вашего VPC
 
   # Входящие правила (ingress)
   ingress {
@@ -22,7 +23,7 @@ resource "aws_security_group" "example_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = [var.home_ip]
+    cidr_blocks = ["91.224.45.78/32"] # Замените <YOUR_HOME_IP> на ваш домашний IP
   }
 
   ingress {
@@ -30,7 +31,7 @@ resource "aws_security_group" "example_sg" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"] # Разрешить всем
   }
 
   ingress {
@@ -38,7 +39,7 @@ resource "aws_security_group" "example_sg" {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"] # Разрешить всем
   }
 
   # Исходящие правила (egress)
@@ -46,12 +47,19 @@ resource "aws_security_group" "example_sg" {
     description = "Allow all outbound traffic"
     from_port   = 0
     to_port     = 0
-    protocol    = "-1"
+    protocol    = "-1" # Все протоколы
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
-
 resource "aws_key_pair" "terraform_ec2_key" {
-  key_name   = var.key_name
-  public_key = file(var.public_key_file)
+  key_name   = "terraform_ec2_key"
+  public_key = file("terraform_ec2_key.pub")
+
 }
+
+output "instance_ip" {
+  value       = aws_instance.dev_ops_week2
+  description = "Public IP address of the EC2 instance"
+}
+
+
